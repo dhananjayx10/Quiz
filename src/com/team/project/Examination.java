16af5534bece8c516f2d014d11627ddf363ef7e7
Dhananjay <dhananjaysonule10@gmail.com>
2024-06-02 19:37:50 +0530

    first commit

---
+++ src/com/team/project/Examination.java
@@ -0,0 +1,23 @@
+package com.team.project;
+
+public class Examination {
+
+	public static void main(String[] args) {
+		Examination obj = new Examination();
+		obj.details();
+		obj.question();
+	}
+
+	// login method
+	public void details() {
+		System.out.println("login");
+	}
+
+	// -------------------------------------
+
+	// question method
+	public void question() {
+		System.out.println("question");
+
+	}
+}
